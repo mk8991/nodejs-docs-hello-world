90c029d02a40a4f6b5ea4abb97d461863850f52e
mk8991 <30465947+mk8991@users.noreply.github.com>
2018-04-06 23:06:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! This is another build and its build no11");
+    response.end("Hello World! This is another build and its build for docker");
 
 });
 
